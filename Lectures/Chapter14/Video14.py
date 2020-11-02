9667c35e4b9b83fc8048140af61cc8c5e641fadd
Nevin Cansel <nevincansel@Nevins-MacBook-Pro.local>
2020-11-02 23:29:40 +0300

    honor course

--- Lectures/Chapter14/Video14.py
+++ Lectures/Chapter14/Video14.py
@@ -1,3 +1,8 @@
 print(ord('G')) # the ord() function tells us the numeric value of a simple ASCII character.
 print(ord('*'))
 print(ord('\n'))
+
+total = 0
+for abc in range(5):
+    total = total + abc
+print(total)
